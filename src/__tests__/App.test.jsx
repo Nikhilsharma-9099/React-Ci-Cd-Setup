1d8d13f4f847045546e72c5994ee1d238460f552
Nikhil Sharma <NikhilSharma@example.com>
2025-09-06 17:02:32 +0530

    Simplify tests: remove unnecessary act() usage

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,26 +1,19 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import { act } from "react-dom/test-utils";
 import App from '../App';
 
+
 test('renders Vite + React text', () =>{
-    act(() => {
+
     render(<App />);
-  });
     const headerElement = screen.getByText(/Vite \+ React/i);
     expect(headerElement).toBeInTheDocument();
 });
 
 test('increments count on button click', () => {
-    act(() => {
-    render(<App />);
-  });
-    const buttonElement = screen.getByText(/count is 0/i);
-    expect(buttonElement).toBeInTheDocument();
 
-    fireEvent.click(buttonElement);
-    expect(buttonElement).toHaveTextContent('count is 1');
-
-    fireEvent.click(buttonElement);
-    expect(buttonElement).toHaveTextContent('count is 2');
+  render(<App />);
+  const buttonElement = screen.getByText(/count is 0/i);
+  fireEvent.click(buttonElement);
+  expect(screen.getByText(/count is 1/i)).toBeInTheDocument();
 
 })
