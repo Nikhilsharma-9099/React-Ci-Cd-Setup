6b986220d31d37789ea397933b34388c87bb58fb
Nikhil Sharma <NikhilSharma@example.com>
2025-09-06 17:47:49 +0530

    Added new dependencies

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,19 +1,15 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from '../App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "../App";
 
-
-test('renders Vite + React text', () =>{
-
-    render(<App />);
-    const headerElement = screen.getByText(/Vite \+ React/i);
-    expect(headerElement).toBeInTheDocument();
+test("renders Vite + React text", () => {
+  render(<App />);
+  expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
 });
 
-test('increments count on button click', () => {
-
+test("increments count on button click", () => {
   render(<App />);
-  const buttonElement = screen.getByText(/count is 0/i);
-  fireEvent.click(buttonElement);
+  const button = screen.getByText(/count is 0/i);
+  fireEvent.click(button);
   expect(screen.getByText(/count is 1/i)).toBeInTheDocument();
-
-})
+});
