f65721e9f72655b203b5bfe84432f35a4e7b4fe5
Nikhil Sharma <NikhilSharma@example.com>
2025-09-06 16:20:06 +0530

    Added new changes

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,14 +1,19 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import { act } from "react";
 import App from '../App';
 
 test('renders Vite + React text', () =>{
+    act(() => {
     render(<App />);
+  });
     const headerElement = screen.getByText(/Vite \+ React/i);
     expect(headerElement).toBeInTheDocument();
 });
 
 test('increments count on button click', () => {
+    act(() => {
     render(<App />);
+  });
     const buttonElement = screen.getByText(/count is 0/i);
     expect(buttonElement).toBeInTheDocument();
 
